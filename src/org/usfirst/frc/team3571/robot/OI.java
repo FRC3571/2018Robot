894e501b693937a665085a90046fbd7f22cd2e29
Scarger <supremeterminal@gmail.com>
2018-01-12 07:10:12 -0500

    added motors branch to work off of

--- src/org/usfirst/frc/team3571/robot/OI.java
+++ src/org/usfirst/frc/team3571/robot/OI.java
@@ -1,47 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package org.usfirst.frc.team3571.robot;
 
-import org.usfirst.frc.team3571.robot.utilities.*;
+import edu.wpi.first.wpilibj.XboxController;
+import org.usfirst.frc.team3571.robot.commands.Autonomous;
+//import org.usfirst.frc.team3571.robot.utilities.XboxController;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI extends RobotMap {
-
-	public static final XboxController driver = new XboxController(DriverUSB.DRIVER_CONTROLLER, DEFAULT.CONTROLLER_DEADZONE);
-	public static final XboxController operator = new XboxController(DriverUSB.OPERATOR_CONTROLLER, DEFAULT.CONTROLLER_DEADZONE);
-
-	public static final RobotDrive drive = new RobotDrive(PWM.FRONT_LEFT_DRIVE_MOTOR, PWM.REAR_LEFT_DRIVE_MOTOR,
-			PWM.FRONT_RIGHT_DRIVE_MOTOR,PWM.REAR_RIGHT_DRIVE_MOTOR);
-	//public static final Talon shooter = new Talon(PWM.SHOOTER);
-	//public static final Talon intake = new Talon(PWM.INTAKE);
-	//public static final Talon climber1 = new Talon(PWM.FRONT_CLIMBER);
-	//public static final Talon climber2 = new Talon(PWM.REAR_CLIMBER);
-	public static final AnalogInput proximityAnalog = new AnalogInput(Analog.PROXIMITY_ANALOG);
-	public static final DigitalOutput agitator = new DigitalOutput(Digital.AGITATOR);
-	public static final DigitalInput limit_button = new DigitalInput(Digital.LIMIT_GEAR);
-	public static final CameraModule cameras = new CameraModule ();
-	/**
-	 * Calls All Refresh Methods
-	 */
+public class OI {
+	//private Joystick m_joystick = new Joystick(0);
+	public static final int DRIVER_CONTROLLER_PORT = 0;
 	
-	public static final Gyro gyro = new ADXRS450_Gyro();
+	public static final XboxController driver = new XboxController(DRIVER_CONTROLLER_PORT);
 	
-	public static void refreshAll() {
-		driver.refresh();
-		operator.refresh();
+	public OI() {
+		// Put Some buttons on the SmartDashboard
+		
+		SmartDashboard.putData("Deliver Soda", new Autonomous());
+
+		// Create some buttons
+		/*
+		JoystickButton dpadUp = new JoystickButton(m_joystick, 5);
+		JoystickButton dpadRight = new JoystickButton(m_joystick, 6);
+		JoystickButton dpadDown = new JoystickButton(m_joystick, 7);
+		JoystickButton dpadLeft = new JoystickButton(m_joystick, 8);
+		JoystickButton l2 = new JoystickButton(m_joystick, 9);
+		JoystickButton r2 = new JoystickButton(m_joystick, 10);
+		JoystickButton l1 = new JoystickButton(m_joystick, 11);
+		JoystickButton r1 = new JoystickButton(m_joystick, 12);
+
+		// Connect the buttons to commands
+		dpadUp.whenPressed(new SetElevatorSetpoint(0.2));
+		dpadDown.whenPressed(new SetElevatorSetpoint(-0.2));
+		dpadRight.whenPressed(new CloseClaw());
+		dpadLeft.whenPressed(new OpenClaw());
+
+		r1.whenPressed(new PrepareToPickup());
+		r2.whenPressed(new Pickup());
+		l1.whenPressed(new Place());
+		l2.whenPressed(new Autonomous());
+		*/
 	}
-	
-	/** Returns distance in mm **/
-	public static double getDistance(){
-		return ((double)proximityAnalog.getValue() * 1.24941) + 0.10889;
+
+	public XboxController getJoystick() {
+		return driver;
 	}
 }
--- src/org/usfirst/frc/team3571/robot/Robot.java
+++ src/org/usfirst/frc/team3571/robot/Robot.java
@@ -1,23 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team3571.robot;
-import org.usfirst.frc.team3571.robot.command.DefaultAuto;
-import org.usfirst.frc.team3571.robot.command.LeftStart;
-import org.usfirst.frc.team3571.robot.command.RightStart;
-import org.usfirst.frc.team3571.robot.command.CenterStart;
-import org.usfirst.frc.team3571.robot.command.MyAuto;
-import org.usfirst.frc.team3571.robot.command.GyroTest;
 
-import edu.wpi.cscore.AxisCamera;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+//import edu.wpi.first.wpilibj.Spark;
+//import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+//import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team3571.robot.commands.Autonomous;
+import org.usfirst.frc.team3571.robot.subsystems.DriveTrain;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
 public class Robot extends IterativeRobot {
-	private AxisCamera camera;
-	SendableChooser<Command> chooser;
-	Command auto;
+	Command m_autonomousCommand;
+
+	public static DriveTrain m_drivetrain;
+	public static OI m_oi;
+	
+	//Six motor drivetrain:	 
+	/*
+	Spark m_frontLeft = new Spark(1);
+	Spark m_midLeft = new Spark(2);
+	Spark m_rearLeft = new Spark(3);
+	SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_midLeft, m_rearLeft);
+
+	Spark m_frontRight = new Spark(4);
+	Spark m_midRight = new Spark(5);
+	Spark m_rearRight = new Spark(6);
+	SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_midRight, m_rearRight);
+
+	DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+	*/	 
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -25,61 +53,61 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		camera = CameraServer.getInstance().addAxisCamera(RobotMap.IP.CAMERA);
-		
-		chooser = new SendableChooser<Command>();
-		chooser.addDefault("MyAuto", new MyAuto());
-		chooser.addObject("Left Start", new LeftStart());
-		chooser.addObject("Right Start", new RightStart());
-		chooser.addObject("Center Start", new CenterStart());
-		chooser.addObject("Default Auto", new DefaultAuto());
-		chooser.addObject("GyroTest", new GyroTest());
-		SmartDashboard.putData("Auto choices", chooser);
+		// Initialize all subsystems
+		m_drivetrain = new DriveTrain();
+		m_oi = new OI();
+
+		// instantiate the command used for the autonomous period
+		m_autonomousCommand = new Autonomous();
+
+		// Show what command your subsystem is running on the SmartDashboard
+		SmartDashboard.putData(m_drivetrain);
 		
 	}
 
-	/**
-	 * This is called once when the autonomous mode is started
-	 */
 	@Override
 	public void autonomousInit() {
-		auto = chooser.getSelected();
-		auto.start();
+		m_autonomousCommand.start(); // schedule the autonomous command (example)
 	}
 
 	/**
-	 * This function is called periodically during autonomous
+	 * This function is called periodically during autonomous.
 	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		log();
 	}
 
-	/**
-	 * This is called once when the Teleop mode is started
-	 */
 	@Override
 	public void teleopInit() {
-		Teleop.init();
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		m_autonomousCommand.cancel();
 	}
 
 	/**
-	 * This function is called periodically during operator control at a maximum
-	 * rate of 50Hz
+	 * This function is called periodically during teleoperated mode.
 	 */
 	@Override
 	public void teleopPeriodic() {
-		OI.refreshAll();
-		Teleop.periodic();
+		Scheduler.getInstance().run();
+		log();
 	}
 
 	/**
-	 * This function is called periodically during test mode
+	 * This function is called periodically during test mode.
 	 */
 	@Override
 	public void testPeriodic() {
-		// Put code you want to test here
-		// It is recommended to put functions here in order to keep it clean
 	}
-    
+
+	/**
+	 * The log method puts interesting information to the SmartDashboard.
+	 */
+	private void log() {
+		m_drivetrain.log();
+	}
 }
