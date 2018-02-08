f966754c3611a8d6458916aa3a66be342dcabbc4
scarger <supremeterminal@gmail.com>
2018-02-08 15:49:53 -0500

    Update Robot.java

--- src/org/usfirst/frc/team3571/robot/Robot.java
+++ src/org/usfirst/frc/team3571/robot/Robot.java
@@ -1,85 +1,135 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team3571.robot;
-import org.usfirst.frc.team3571.robot.command.DefaultAuto;
-import org.usfirst.frc.team3571.robot.command.LeftStart;
-import org.usfirst.frc.team3571.robot.command.RightStart;
-import org.usfirst.frc.team3571.robot.command.CenterStart;
-import org.usfirst.frc.team3571.robot.command.MyAuto;
-import org.usfirst.frc.team3571.robot.command.GyroTest;
 
-import edu.wpi.cscore.AxisCamera;
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-public class Robot extends IterativeRobot {
-	private AxisCamera camera;
-	SendableChooser<Command> chooser;
-	Command auto;
+import org.usfirst.frc.team3571.robot.commands.Autonomous;
+import org.usfirst.frc.team3571.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team3571.robot.utilities.MPU6050;
+import org.usfirst.frc.team3571.robot.utilities.XboxController;
+import org.usfirst.frc.team3571.robot.utilities.XboxController.StickSides;
 
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	Command m_autonomousCommand;
+	
+	public static DriveTrain m_drivetrain;
+	public static OI m_oi;
+	public static XboxController xbox;
+	public MPU6050 gyro = new MPU6050();
+
+    //private Compressor c = new Compressor(0);
+	private Encoder enc = new Encoder(0,1);
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
-		camera = CameraServer.getInstance().addAxisCamera(RobotMap.IP.CAMERA);
 		
-		chooser = new SendableChooser<Command>();
-		chooser.addDefault("MyAuto", new MyAuto());
-		chooser.addObject("Left Start", new LeftStart());
-		chooser.addObject("Right Start", new RightStart());
-		chooser.addObject("Center Start", new CenterStart());
-		chooser.addObject("Default Auto", new DefaultAuto());
-		chooser.addObject("GyroTest", new GyroTest());
-		SmartDashboard.putData("Auto choices", chooser);
+		
+		// Initialize all subsystems
+		m_drivetrain = new DriveTrain();
+		m_oi = new OI();
+
+		// instantiate the command used for the autonomous period
+		m_autonomousCommand = new Autonomous();
+		xbox = m_oi.getXboxControl();
+
+		// Show what command your subsystem is running on the SmartDashboard
+		SmartDashboard.putData(m_drivetrain);
 		
 	}
 
-	/**
-	 * This is called once when the autonomous mode is started
-	 */
 	@Override
 	public void autonomousInit() {
-		auto = chooser.getSelected();
-		auto.start();
+		m_autonomousCommand.start(); // schedule the autonomous command (example)
 	}
 
 	/**
-	 * This function is called periodically during autonomous
+	 * This function is called periodically during autonomous.
 	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		log();
 	}
 
-	/**
-	 * This is called once when the Teleop mode is started
-	 */
 	@Override
 	public void teleopInit() {
-		Teleop.init();
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		m_autonomousCommand.cancel();		
 	}
 
 	/**
-	 * This function is called periodically during operator control at a maximum
-	 * rate of 50Hz
+	 * This function is called periodically during teleoperated mode.
 	 */
 	@Override
 	public void teleopPeriodic() {
-		OI.refreshAll();
-		Teleop.periodic();
+		m_oi.refreshAll();
+		Scheduler.getInstance().run();
+		log();
 	}
 
 	/**
-	 * This function is called periodically during test mode
+	 * This function is called periodically during test mode.
 	 */
 	@Override
-	public void testPeriodic() {
-		// Put code you want to test here
-		// It is recommended to put functions here in order to keep it clean
+	public void testPeriodic(){
+		//Testing xbox buttons, joysticks and triggers
+		m_oi.refreshAll();
+		//System.out.println("Left Y = " + xbox.LeftStick.Y);
+		//System.out.println("Right Y = " + xbox.RightStick.Y);
+		//System.out.println("Left X = " + xbox.LeftStick.X);
+		//System.out.println("Right X = " + xbox.RightStick.X);
+		if(xbox.Buttons.A.changedDown){
+			System.out.println("Button A = " + xbox.Buttons.A.changedDown);
+		} else if(xbox.Buttons.B.changedUp){
+			System.out.println("Button B = " + xbox.Buttons.B.changedDown);
+		}
+		
+		
+		final double cpr = 360.0;
+		final double encoder_angular_distance_per_pulse = 2.0*Math.PI / cpr;
+		final double wheel_radius = 2.5;  // .564 in (18T 5mm pitch)
+		final double encLinearDistancePerPulse = wheel_radius * encoder_angular_distance_per_pulse; //2.0 * Math.PI / cpr;
+		
+		System.out.println(enc.getRaw()*encLinearDistancePerPulse);
+		
+		
+		//System.out.println(gyro.getAngle(MPU6050.Axis.X));
+
+	    //PNEUMATICS
+	    //c.setClosedLoopControl(true);
+	    //c.setClosedLoopControl(false);
 	}
-    
+	
+
+	/**
+	 * The log method puts interesting information to the SmartDashboard.
+	 */
+	private void log() {
+		m_drivetrain.log();
+	}
 }
